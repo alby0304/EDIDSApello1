eca99ad9d666083b79713ea00cd46daba085ce30
Alberto Bettini <alberto.bettini@marconirovereto.it>
2024-06-03 18:18:24 +0200

    rimozione codice commentato

--- src/myTest/ListAdapterTest.java
+++ src/myTest/ListAdapterTest.java
@@ -1,17 +1,16 @@
 package myTest;
-import myAdapter.ListAdapter;
-import myAdapter.ListIteratorAdapter;
+import myAdapter.*;
 
 public class ListAdapterTest {
     public static void main(String[] args) {
-        ListAdapter<Integer> list = new ListAdapter<>();
+        ListAdapter list = new ListAdapter();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
         System.out.println("Lista: " + list);
-        ListIteratorAdapter<Integer> listIterator = new ListIteratorAdapter<>(list);
+        ListIteratorAdapter listIterator = new ListIteratorAdapter(list);
         System.out.println("ListaIterator: " + listIterator);
         System.out.println("HasNext: " + listIterator.hasNext());
         System.out.println("NextIndex: " + listIterator.nextIndex());
@@ -19,12 +18,19 @@ public class ListAdapterTest {
         System.out.println("HasPrevious: " + listIterator.hasPrevious());
         System.out.println("PreviousIndex: " + listIterator.previousIndex());
         System.out.println("Previous: " + listIterator.previous());
+        System.out.println("Previous2: " + listIterator.previous());
+        System.out.println("Next: " + listIterator.next());
         listIterator.add(6);
         System.out.println("Lista: " + list);
         System.out.println("ListaIterator: " + listIterator);
         listIterator.set(7);
         System.out.println("Lista: " + list);
         System.out.println("ListaIterator: " + listIterator);
+        
+        for (Object element : list.toArray()) {
+            System.out.println(element);
+        }
+
     }
 }
 
