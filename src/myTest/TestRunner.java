9683a3be813a4be7d75d1d60658c290b47001e65
Alberto Bettini <alberto.bettini@marconirovereto.it>
2024-06-17 19:35:14 +0200

    Aggiunta test suite

--- src/myTest/TestRunner.java
+++ src/myTest/TestRunner.java
@@ -2,16 +2,23 @@ package myTest;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ListAdapterTest.class,
+        ListIteratorAdapterTest.class,
+})
 public class TestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(ListAdapterTest.class);
+        Result result = JUnitCore.runClasses(ListAdapterTest.class, ListIteratorAdapterTest.class );
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
-        System.out.println("Total tests run: " + result.getRunCount());
-        System.out.println("Total tests failed: " + result.getFailureCount());
-        System.out.println("Total time: " + result.getRunTime() + "ms");
+        System.out.println("\nTotal tests run: " + result.getRunCount());
+        System.out.println("\nTotal tests failed: " + result.getFailureCount());
+        System.out.println("\nTotal time: " + result.getRunTime() + "ms");
     }
 }
